3890bed3cb1b815e950ef97c7464122a0d421962
dhaneesh <dhaneesh@salescode.ai>
2024-03-05 11:27:52 +0530

    changed

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 kill -9 `cat scaleguard.pid`
-nohup java -DadminUser=scaleguard -DadminPassword=Scaleguard123$ -jar target/scaleguard-1.0-SNAPSHOT.jar > scaleguard.log & echo $! > scaleguard.pid
+nohup java -DadminUser=scaleguard -DadminPassword=Scaleguard123$ -jar scaleguard.jar > scaleguard.log & echo $! > scaleguard.pid

7d601ca9eb2d19e3e3f320f8e75e6ef4ec742b49
dhaneesh <dhaneesh@salescode.ai>
2024-03-16 14:55:24 +0530

    changed

--- src/main/java/com/scaleguard/server/dns/DNSAddressBook.java
+++ src/main/java/com/scaleguard/server/dns/DNSAddressBook.java
@@ -12,7 +12,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class DNSAddressBook {
-    private DNSAddressBook(){
+
+    static {
         load();
     }
     public static class WrappedDNSRecord{
